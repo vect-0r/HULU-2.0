be0ed69fd9fb508b484c062b9abbd497ab4e7321
Roopam Jain <roopam.ams@gmail.com>
2021-06-20 01:37:54 +0530

    Created the backend request API and Built the Navbar Component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Header />
 
       {/* Navbar */}
+      <Nav />
 
       {/* Results */}
     </div>
