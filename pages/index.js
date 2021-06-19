2a2090a9864126cda717786221a427d2118e9876
Roopam Jain <roopam.ams@gmail.com>
2021-06-19 23:11:27 +0530

    Built the header items, icons added

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Let's build Hulu 2.0</h1>
+      {/* <h1>Let's build Hulu 2.0</h1> */}
 
       {/* Header */}
       <Header />
